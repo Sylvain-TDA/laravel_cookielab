c18c2f6ed40c8202cb5030b0b3abcf7dcd653d99
Sylvain-TDA <sylvain.teissier@le-campus-numerique.fr>
2025-07-23 15:32:38 +0200

    bandeau promos

---
+++ resources/views/bandeau.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="d-flex flex-row flex-fill justify-content-center mb-3" style="background-color: #F0E2CC;">
+        <!-- <div class="dropdown flex-grow-1">
+            <button class="btn dropdown-toggle text-light" style="background-color: #B66E00;"
+            type="button"
+            id="dropdownMenu2"
+            data-bs-toggle="dropdown"
+            aria-expanded="false">
+                Langue
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                <button class="dropdown-item" type="button">Français</button>
+                <button class="dropdown-item" type="button">Anglais</button>
+                <button class="dropdown-item" type="button">Autre</button>
+            </div>
+        </div> -->
+        <div class="p-2 flex-grow-1">Livraison gratuite dès 60€ d'achat</div>
+        <div class="p-2 flex-grow-1">-5% avec le code MONSTAH</div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\bandeaupromotions;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/bandeau', [BandeauPromotions::class, 'show']);
