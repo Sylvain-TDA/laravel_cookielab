20146f79b9f074ebff87e2edf35fa32e9ced6a7d
Sylvain-TDA <sylvain.teissier@le-campus-numerique.fr>
2025-07-24 14:45:07 +0200

    modification footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,26 +1,34 @@
-<footer class="text-center text-lg-start"
-    style="background-color: rgba(182, 110, 0, 0.8); backdrop-filter: blur(10px); -webkit-backdrop-filter: blur(10px);">
-    <div class="container d-flex justify-content-center py-5">
-        <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #ffedd3;">
-            <i class="fab fa-facebook-f" style="color: black"></i>
-        </button>
-        <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #ffedd3;">
-            <i class="fab fa-youtube" style="color: black"></i>
-        </button>
-        <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #ffedd3;">
-            <i class="fab fa-instagram" style="color: black"></i>
-        </button>
-        <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #ffedd3;">
-            <i class="fab fa-x" style="color: black"></i>
-        </button>
-    </div>
+<div style="position:relative;min-height:35vh;">
+    <div style="padding-bottom: 2.5rem;">
+        <footer class="text-center text-lg-start"
+            style="background-color: rgba(182, 110, 0, 0.8); backdrop-filter: blur(10px); -webkit-backdrop-filter: blur(10px); position:absolute; bottom:0; width: 100%;">
+            <div class="container d-flex justify-content-center py-5">
+                <button type="button" class="btn btn-primary btn-lg btn-floating mx-2"
+                    style="background-color: #ffedd3;">
+                    <i class="fab fa-facebook-f" style="color: black"></i>
+                </button>
+                <button type="button" class="btn btn-primary btn-lg btn-floating mx-2"
+                    style="background-color: #ffedd3;">
+                    <i class="fab fa-youtube" style="color: black"></i>
+                </button>
+                <button type="button" class="btn btn-primary btn-lg btn-floating mx-2"
+                    style="background-color: #ffedd3;">
+                    <i class="fab fa-instagram" style="color: black"></i>
+                </button>
+                <button type="button" class="btn btn-primary btn-lg btn-floating mx-2"
+                    style="background-color: #ffedd3;">
+                    <i class="fab fa-x" style="color: black"></i>
+                </button>
+            </div>
 
-    <!-- Copyright -->
-    <div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-        © 2025 Copyright:
-        <a class="text-white" href="">CookieLab.com</a>
+            <!-- Copyright -->
+            <div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+                © 2025 Copyright:
+                <a class="text-white" href="">CookieLab.com</a>
+            </div>
+        </footer>
     </div>
-</footer>
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.3.2/mdb.min.js"></script>
