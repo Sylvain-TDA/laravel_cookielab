9e9ae3d914002bb27a73dea7e42282271cd79cad
Léa Deffaisse <lea.deffaisse@le-campus-numerique.fr>
2025-07-24 12:10:20 +0200

    breadcrumb

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+    public function index()
+    {
+        $products = collect([
+            (object)[
+                'id' => 1,
+                'name' => 'iPhone 15',
+                'price' => 999.99,
+                'description' => 'Le dernier smartphone d\'Apple avec une caméra révolutionnaire et des performances exceptionnelles.'
+            ],
+            (object)[
+                'id' => 2,
+                'name' => 'Samsung Galaxy S24',
+                'price' => 899.99,
+                'description' => 'Smartphone Android haut de gamme avec écran AMOLED et intelligence artificielle intégrée.'
+            ],
+            (object)[
+                'id' => 3,
+                'name' => 'Google Pixel 8',
+                'price' => 799.99,
+                'description' => 'Le meilleur de Google dans un smartphone, avec une photographie assistée par IA.'
+            ]
+        ]);
+
+        $breadcrumbs = [
+            ['title' => 'Accueil', 'url' => route('accueil')],
+            ['title' => 'Produits', 'url' => '']
+        ];
+
+        return view('products.product', compact('products', 'breadcrumbs'));
+    }
+
+    //
+    public function show($id)
+    {
+        $products = collect([
+            (object)[
+                'id' => 1,
+                'name' => 'iPhone 15',
+                'price' => 999.99,
+                'description' => 'Le dernier smartphone d\'Apple avec une caméra révolutionnaire et des performances exceptionnelles.'
+            ],
+            (object)[
+                'id' => 2,
+                'name' => 'Samsung Galaxy S24',
+                'price' => 899.99,
+                'description' => 'Smartphone Android haut de gamme avec écran AMOLED et intelligence artificielle intégrée.'
+            ],
+            (object)[
+                'id' => 3,
+                'name' => 'Google Pixel 8',
+                'price' => 799.99,
+                'description' => 'Le meilleur de Google dans un smartphone, avec une photographie assistée par IA.'
+            ]
+        ]);
+
+        $product = $products->get($id);
+        
+        if (!$product) {
+            abort(404);  // Produit non trouvé
+        }
+
+        $breadcrumbs = [
+            ['title' => 'Accueil', 'url' => route('accueil')],
+            ['title' => 'Produits', 'url' => route('catalogue')],
+            ['title' => $product->name, 'url' => '']
+        ];
+
+        return view('products.product-show', compact('product', 'breadcrumbs'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,13 @@ use App\Http\Controllers\AddToCart;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\bandeaupromotions;
 
+
 Route::get('/', function () {
-    return view('welcome');  // Charge resources/views/welcome.blade.php
-});
+    return view('welcome'); 
+})->name('accueil');
 
+//Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('welcome');
 
+Route::get('/products', [App\Http\Controllers\ProductController::class, 'index'])->name('catalogue');
+
+Route::get('/products/{id}', [App\Http\Controllers\ProductController::class, 'show'])->name('products.show');
