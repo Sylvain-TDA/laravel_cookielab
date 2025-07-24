635cad70e184b38000509b417c22277053301e3c
Sylvain-TDA <sylvain.teissier@le-campus-numerique.fr>
2025-07-24 09:41:54 +0200

    WIP-basketpage

---
+++ resources/views/basket/basket-show.blade.php
@@ -0,0 +1,211 @@
+@extends('layouts.basket')
+@section('basket-content')
+
+    <section class="h-100">
+        <div class="container h-100 py-5">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-10">
+
+                    <div class="d-flex justify-content-between align-items-center mb-4">
+                        <h3 class="fw-normal mb-0" style="color: #B66E00">Mon panier</h3>
+                    </div>
+
+                    <div class="card rounded-3 mb-4">
+                        <div class="card-body p-4">
+                            <div class="row d-flex justify-content-between align-items-center">
+                                <div class="col-md-2 col-lg-2 col-xl-2">
+                                    <img src="images/kit.png" class="img-fluid rounded-3" alt="Cotton T-shirt">
+                                </div>
+                                <div class="col-md-3 col-lg-3 col-xl-3">
+                                    <p class="lead fw-normal mb-2">Kit cookies</p>
+                                    <p>Description de nos kits cookies super trop cool</p>
+                                </div>
+                                <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
+                                    <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
+                                        onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
+                                        <i class="fas fa-minus"></i>
+                                    </button>
+
+                                    <input id="form1" min="0" name="quantity" value="2" type="number"
+                                        class="form-control form-control-sm" />
+
+                                    <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
+                                        onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
+                                        <i class="fas fa-plus"></i>
+                                    </button>
+                                </div>
+                                <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+                                    <h5 class="mb-0">19.99€</h5>
+                                </div>
+                                <div class="col-md-1 col-lg-1 col-xl-1 text-end">
+                                    <a href="#!" class="text-danger"><i class="fas fa-trash fa-lg"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card rounded-3 mb-4">
+                        <div class="card-body p-4">
+                            <div class="row d-flex justify-content-between align-items-center">
+                                <div class="col-md-2 col-lg-2 col-xl-2">
+                                    <img src="images/kit.png" class="img-fluid rounded-3" alt="Cotton T-shirt">
+                                </div>
+                                <div class="col-md-3 col-lg-3 col-xl-3">
+                                    <p class="lead fw-normal mb-2">Kit cookies</p>
+                                    <p>Description de nos kits cookies super trop cool</p>
+                                </div>
+                                <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
+                                    <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
+                                        onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
+                                        <i class="fas fa-minus"></i>
+                                    </button>
+
+                                    <input id="form1" min="0" name="quantity" value="2" type="number"
+                                        class="form-control form-control-sm" />
+
+                                    <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
+                                        onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
+                                        <i class="fas fa-plus"></i>
+                                    </button>
+                                </div>
+                                <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+                                    <h5 class="mb-0">19.99€</h5>
+                                </div>
+                                <div class="col-md-1 col-lg-1 col-xl-1 text-end">
+                                    <a href="#!" class="text-danger"><i class="fas fa-trash fa-lg"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card rounded-3 mb-4">
+                        <div class="card-body p-4">
+                            <div class="row d-flex justify-content-between align-items-center">
+                                <div class="col-md-2 col-lg-2 col-xl-2">
+                                    <img src="images/kit.png" class="img-fluid rounded-3" alt="Cotton T-shirt">
+                                </div>
+                                <div class="col-md-3 col-lg-3 col-xl-3">
+                                    <p class="lead fw-normal mb-2">Kit cookies</p>
+                                    <p>Description de nos kits cookies super trop cool</p>
+                                </div>
+                                <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
+                                    <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
+                                        onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
+                                        <i class="fas fa-minus"></i>
+                                    </button>
+
+                                    <input id="form1" min="0" name="quantity" value="2" type="number"
+                                        class="form-control form-control-sm" />
+
+                                    <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
+                                        onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
+                                        <i class="fas fa-plus"></i>
+                                    </button>
+                                </div>
+                                <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+                                    <h5 class="mb-0">19.99€</h5>
+                                </div>
+                                <div class="col-md-1 col-lg-1 col-xl-1 text-end">
+                                    <a href="#!" class="text-danger"><i class="fas fa-trash fa-lg"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card rounded-3 mb-4">
+                        <div class="card-body p-4">
+                            <div class="row d-flex justify-content-between align-items-center">
+                                <div class="col-md-2 col-lg-2 col-xl-2">
+                                    <img src="images/kit.png" class="img-fluid rounded-3" alt="Cotton T-shirt">
+                                </div>
+                                <div class="col-md-3 col-lg-3 col-xl-3">
+                                    <p class="lead fw-normal mb-2">Kit cookies</p>
+                                    <p>Description de nos kits cookies super trop cool</p>
+                                </div>
+                                <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
+                                    <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
+                                        onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
+                                        <i class="fas fa-minus"></i>
+                                    </button>
+
+                                    <input id="form1" min="0" name="quantity" value="2" type="number"
+                                        class="form-control form-control-sm" />
+
+                                    <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
+                                        onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
+                                        <i class="fas fa-plus"></i>
+                                    </button>
+                                </div>
+                                <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+                                    <h5 class="mb-0">19.99€</h5>
+                                </div>
+                                <div class="col-md-1 col-lg-1 col-xl-1 text-end">
+                                    <a href="#!" class="text-danger"><i class="fas fa-trash fa-lg"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card mb-4">
+                        <div class="card-body p-4 d-flex flex-row">
+                            <div data-mdb-input-init class="form-outline flex-fill">
+                                <input type="text" id="form1" class="form-control form-control-lg" />
+                                <label class="form-label" for="form1">Code promo</label>
+                            </div>
+                            <button type="button" data-mdb-button-init data-mdb-ripple-init
+                                class="btn btn-outline-warning btn-lg ms-3"
+                                style="background-color: #B66E00; color: white; border-color : white;">Appliquer</button>
+                        </div>
+                    </div>
+
+                    <div class="card mb-4">
+                        <div class="card-body p-4 d-flex flex-row">
+                            <div data-mdb-input-init class="form-outline flex-fill">
+                                <input type="text" id="form1" class="form-control form-control-lg" />
+                                <label class="form-label" for="form1">Note</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-end">
+                        <button type="button" data-mdb-button-init data-mdb-ripple-init
+                            class="btn btn-warning btn-block btn-lg my-3"
+                            style="background-color: #B66E00; color: white; border-color : white;">Paiement</button>
+                    </div>
+                    <div class="card mb-3">
+                        <div class="card-body p-4 d-flex flex-row">DJ</div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- <h1 style="color: #B66E00 ; margin : 16px">Mon panier</h1>
+                <div class="d-flex flex-row align-items-center p-3 border">
+                    <div class="me-3">
+                        <img src="/images/kit.png" alt="kit de cookies" style="width: 100px; height: auto;">
+                    </div>
+
+                    <div class="d-flex flex-column flex-fill">
+                        <div class="fw-bold">Nom du cookie</div>
+                        <div class="text-muted">Description du cookie</div>
+                    </div>
+
+                    <div class="ms-3 text-end">
+                        <span class="fw-bold">19,99 €</span>
+                    </div>
+                </div>
+                    <div class="d-flex flex-row align-items-center p-3 border">
+                    <div class="me-3">
+                        <img src="/images/kit.png" alt="kit de cookies" style="width: 100px; height: auto;">
+                    </div>
+
+                    <div class="d-flex flex-column flex-fill">
+                        <div class="fw-bold">Nom du cookie</div>
+                        <div class="text-muted">Description du cookie</div>
+                    </div>
+
+                    <div class="ms-3 text-end">
+                        <span class="fw-bold">19,99 €</span>
+                    </div> -->
+@endsection
