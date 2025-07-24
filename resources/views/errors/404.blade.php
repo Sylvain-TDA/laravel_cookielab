6bab2ab58e5e9e679f19b332bc54cdd08c2f8c75
K-pujol <killianpujol26@gmail.com>
2025-07-24 15:01:45 +0200

    modification page 404 ajout background

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,3 +1,14 @@
 <x-header />
-404 - Oups on a mangé tout les cookies ! 🍪
-<x-footer />
+
+
+<div id="intro" class="bg-image shadow-1-strong"
+    style="background-image: url(images/background-404.png); height: 100vh; position: absolute; top: 0; width: 100%;">
+    <div class="mask text-white" style="background-color: rgba(0, 0, 0, 0.6)" data-mdb-theme="dark">
+        <div class="container d-flex align-items-center justify-content-center text-center h-100">
+            <div class="text-white">
+                <h1 class="mb-3">404 - Oups on a mangé tous les cookies ! 🍪</h1>
+
+            </div>
+        </div>
+    </div>
+</div>
