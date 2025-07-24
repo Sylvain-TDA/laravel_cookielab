acc3698249e546363d4fb46ad5721098e2d0ac14
Léa Deffaisse <lea.deffaisse@le-campus-numerique.fr>
2025-07-24 14:03:53 +0200

    update product page

--- resources/views/products/product.blade.php
+++ resources/views/products/product.blade.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title ?? 'Laravel' }}</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.3.2/mdb.min.css" rel="stylesheet">
-</head>
-
-<body>
-    <x-header /> <!-- Pour injecter le header -->
+<x-header /> <!-- Pour injecter le header -->
 <main class="container my-4">
     <x-breadcrumb :items="$breadcrumbs ?? []" />
     @section('title', 'Nos Produits')
@@ -53,10 +39,7 @@
 </div>
 </main>
 
-    <x-footer /> <!-- Pour injecter le footer -->
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.3.2/mdb.min.js"></script>
+<x-footer /> <!-- Pour injecter le footer -->
 </body>
 
 </html>
