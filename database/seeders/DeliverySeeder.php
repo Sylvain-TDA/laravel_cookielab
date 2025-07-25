c986231b340a00c0d1438d59399a4e1ca3c222f2
K-pujol <killianpujol26@gmail.com>
2025-07-25 15:39:28 +0200

    correction seeder avec ajout données multiples

--- database/seeders/DeliverySeeder.php
+++ database/seeders/DeliverySeeder.php
@@ -11,12 +11,14 @@ class DeliverySeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    { {
-            DB::table('products')->insert([
-                'name' => fake()->lastName(),
-                'price' => fake()->randomNumber(2),
-            ]);
+    {   
+        $deliveries = [];
+        for ($i = 0; $i < 4; $i++) {
+            $deliveries[] = [
+                'name' => fake()->word(),
+                'cost' => fake()->randomFloat(2), // Random cost between 1 and 100
+            ];
         }
+        DB::table('deliveries')->insert($deliveries);
     }
-
 }
