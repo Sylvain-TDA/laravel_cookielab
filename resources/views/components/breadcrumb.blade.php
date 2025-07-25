9a59c947fa665c0e987733888a89a5f47d90434f
K-pujol <killianpujol26@gmail.com>
2025-07-25 10:38:38 +0200

    modif font

--- resources/views/components/breadcrumb.blade.php
+++ resources/views/components/breadcrumb.blade.php
@@ -1,19 +1,19 @@
 @props(['items' => []])
 
 @if(count($items) > 0)
-<nav aria-label="breadcrumb" class="my-3">
-    <ol class="breadcrumb">
-        @foreach($items as $item)
-            @if($loop->last)
-                <li class="breadcrumb-item active" aria-current="page">
-                    {{ $item['title'] }}
-                </li>
-            @else
-                <li class="breadcrumb-item">
-                    <a href="{{ $item['url'] }}">{{ $item['title'] }}</a>
-                </li>
-            @endif
-        @endforeach
-    </ol>
-</nav>
+    <nav aria-label="breadcrumb" class="my-3">
+        <ol class="breadcrumb">
+            @foreach($items as $item)
+                @if($loop->last)
+                    <li class="breadcrumb-item active league-gothic" aria-current="page">
+                        {{ $item['title'] }}
+                    </li>
+                @else
+                    <li class="breadcrumb-item league-gothic">
+                        <a href="{{ $item['url'] }}">{{ $item['title'] }}</a>
+                    </li>
+                @endif
+            @endforeach
+        </ol>
+    </nav>
 @endif
