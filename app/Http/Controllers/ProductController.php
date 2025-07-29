53db60375bb2f9f4ab4222124d742713ce708803
Théo <theo.cartegnie@le-campus-numerique.fr>
2025-07-29 12:08:38 +0200

    Remove fake database

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,90 +7,28 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-
-
+   //
     public function index()
     {
-       
-        $products = collect([
-            (object)[
-                'id' => 0,
-                'name' => 'Super dupper cookie',
-                'price' => 2.50,
-                'description' => 'Un cookie au chocolat noir avec des éclats de caramel.'
-            ],
-            (object)[
-                'id' => 1,
-                'name' => 'Cookie aux pépites de chocolat',
-                'price' => 1.99,
-                'description' => 'Un cookie moelleux avec des pépites de chocolat fondant.'
-            ],
-            (object)[
-                'id' => 2,
-                'name' => 'Cookie à la vanille',
-                'price' => 3.49,
-                'description' => 'Un cookie à la vanille avec une texture crémeuse et des éclats de caramel.'
-            ]
-        ]);
-         
+        $products = Product::Where('is_available', true)
+            ->orderBy('name', 'asc')
+            ->get();
         $breadcrumbs = [
             ['title' => 'Accueil', 'url' => route('accueil')],
             ['title' => 'Produits', 'url' => '']
         ];
-
         return view('products.product', compact('products', 'breadcrumbs'));
     }
-
     //
     public function show($id)
     {
-        $products = collect([
-            (object)[
-                'id' => 0,
-                'name' => 'Super dupper cookie',
-                'price' => 2.50,
-                'description' => 'Un cookie au chocolat noir avec des éclats de caramel.',
-                'description_short'=>'Dark chocolate',
-                'description_long' =>'A wonderfull baked cookie full of melty chocolate, Made with the best 
-                 selection of floor and chocolate.This crunchy cookie with light taste of vanilla and darker chocolate than Trump policy 
-                 will make you forget about those of your grand-mother.
-                 Enjoy, folks !',
-            ],
-            (object)[
-                'id' => 1,
-                'name' => 'Cookie aux pépites de chocolat',
-                'price' => 1.99,
-                'description' => 'Un cookie moelleux avec des pépites de chocolat fondant.',
-                'description_short'=>'Dark chocolate',
-                'description_long' =>'A wonderfull baked cookie full of melty chocolate, Made with the best 
-                 selection of floor and chocolate.This crunchy cookie with light taste of vanilla and darker chocolate than Trump policy 
-                 will make you forget about those of your grand-mother.
-                 Enjoy, folks !',
-            ],
-            (object)[
-                'id' => 2,
-                'name' => 'Cookie à la vanille',
-                'price' => 3.49,
-                'description' => 'Un cookie à la vanille avec une texture crémeuse et des éclats de caramel.',
-                'description_short'=>'Dark chocolate',
-                'description_long' =>'A wonderfull baked cookie full of melty chocolate, Made with the best 
-                 selection of floor and chocolate.
-                 This crunchy cookie with light taste of vanilla and darker chocolate than Trump policy 
-                 will make you forget about those of your grand-mother. \r
-                 Enjoy, folks !',
-            ]
-            ]);
+        $product = Product::find($id);
+        if ($product == NULL) {
+            abort(404, 'Produit non trouvé');  // Produit non trouvé
+        }
 
-
-
-   
-
-
-
-        $product = $products->get($id);
-        
-        if (!$product) {
-            abort(404);  // Produit non trouvé
+        if (!$product->is_available) {
+            abort(404, 'Produit indisponible');  // Produit indisponible
         }
 
         $breadcrumbs = [
@@ -98,8 +36,6 @@ class ProductController extends Controller
             ['title' => 'Produits', 'url' => route('catalogue')],
             ['title' => $product->name, 'url' => '']
         ];
-
-
         return view('products.product-show', compact('product', 'breadcrumbs'));
     }
 
