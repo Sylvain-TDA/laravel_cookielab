3088f2b80766638e5d9f52db5699514ac0f8a0bd
Théo <theo.cartegnie@le-campus-numerique.fr>
2025-07-29 12:08:59 +0200

    Update HTML with the new BDD

--- resources/views/products/product-show.blade.php
+++ resources/views/products/product-show.blade.php
@@ -1,57 +1,50 @@
-    <x-header /> <!-- Pour injecter le header -->
+            <x-header /> <!-- Pour injecter le header -->
 
-<main class="container">
+            <main class="container">
 
- <x-breadcrumb :items="$breadcrumbs ?? []" />
+                <x-breadcrumb :items="$breadcrumbs ?? []" />
 
-    <div class = "d-flex flex-column bd-hightlight mb-3">
-        <div class="d-flex">
-        <img src="/images/cookie.png" width="600px" height="auto" alt="Responsive image"/>
-        <div>
-            <h1 class = "m-1 p-1" style = >{{ $product->name }}</h1>
-            
-            <br>
-            <h2>{{ $product->description_short }}</h2>
-            <br>
+                <div class="d-flex flex-column bd-hightlight mb-3">
+                    <div class="d-flex">
+                        <img src="/images/cookie.png" width="600px" height="auto" alt="Responsive image" />
+                        <div>
+                            <h1 class="m-1 p-1 rounded" style="background-color : rgba(229,171,105,255)">{{ $product->name }}</h1>
 
-            <h3 class = "m-1 p-1" style = "">
-                   {{ $product->description_long }}
-            </h3>
-    </div>
- 
-</div>
+                            <br>
+                            <h2>{{ $product->description_short}}</h2>
+                            <br>
 
-        <div div class="d-flex flex-row">
-            <div> <!--Button lot-->
-                <select>
-                    @for($i = 1; $i <= 5; $i++)
-                        <option value="test"> Lot de {{$i}} </option>
-                    @endfor
-                </select>
+                            <h3 class="m-1 p-1 rounded" style="background-color : rgba(229,171,105,255)">
+                                {{ $product->description_long}}
+                            </h3>
+
+
+                            <div div class="d-flex flex-row rounded">
+                                <div style="width : 130px; height : 60px;"> <!--Button lot-->
+                                    <select class="rounded" style="width : 130px; height : 60px;">
+                                        @for($i = 1; $i <= 5; $i++)
+                                            <option class="text-center" value="test"> Lot de {{$i}} </option>
+                                            @endfor
+                                    </select>
+                                </div>
+
+                                <div class="d-flex flex-row mx-1 px-0 rounded border border-dark" > <!-- Button quantité -->
+                                    <button class="rounded" style="width: 60px; height : 60px"> <b>-</b> </button>
+                                        <h1 class="d-flex mx-3">1</h1>
+                                    <button class="rounded" style="width: 60px; height : 60px"> <b>+</b> </button>
+                                </div>
+
+                                <form action="{{ route('basketForm') }}" method="POST">
+                                    @csrf <!-- Protection contre les attaques CSRF -->
+                                    <button class="rounded" type="submit" style="width : 130px; height : 60px; background-color : rgba(189,125,26,255)" disabled>
+                                        Validation de l'achat
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                </div>
+            </main>
 
-                <div class = "d-flex flex-row mx-3 px-0"> <!-- Button quantité -->
-                <form action="{{ route('basketForm') }}" method="POST">
-                       @csrf <!-- Protection contre les attaques CSRF -->
-                        <label for="Number of cookies">Number of cookies</label>
-                        
-                        <label for="name">Nom du produit :</label>
-                        <input type="text" name="name" id="name">
-
-                        <label for="price">Prix :</label>
-                        <input type="number" id="price" name="price" min="1" max="10"/>
-                            
-                        <button type ="submit" style = "width : 130px; height : 60px; background-color : rgba(255,0,255,255)">
-                        Validation de l'achat
-                        </button>   
-                    </form>
-                </div>               
-            </div>
-        </div>
-    </div>
-</main>
-
-<x-footer /> <!-- Pour injecter le footer -->
-
-
-
+            <x-footer /> <!-- Pour injecter le footer -->
